29be5a6b15756efe6fec2ffbd0c809c13bdbc360
root <root@l0k1>
2019-01-02 12:13:50 -0700

    need to do speed test

--- file_manip/speed_test.sh
+++ file_manip/speed_test.sh
@@ -1,6 +1,6 @@
-iters=1000
+iters=$1
 outfile='./speed_results.txt'
 echo 'os.walk ' >> $outfile
-python -m timeit -n $iters 'from file_manip import find_files' 'find_files()'
+python -m timeit -n $iters 'from file_manip import find_files' 'find_files()' >> $outfile
 echo 'system find ' >> $outfile
-python -m timeit -n $iters 'from file_manip import _find_files' '_find_files()'
+python -m timeit -n $iters 'from file_manip import _find_files' '_find_files()' >> $outfile
