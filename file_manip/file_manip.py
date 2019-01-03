c590626930f57ec92df23e8a01690f26d4058800
deadlift1226 <danemorgan91@gmail.com>
2019-01-02 17:17:11 -0700

    works, running speed test

--- file_manip/file_manip.py
+++ file_manip/file_manip.py
@@ -4,20 +4,7 @@ from pprint import pprint
 from os import walk
 #from paramiko import *
 from Flag import Flag, find_flag, make_flags
-from sys import maxsize, getsizeof
 
-
-def _find_files(fname='crazy_flag', loc= '/'):
-    cmd = " ".join(['find', loc, ' -name ', '\"*'+ fname + '*\"', ])
-    fnames = check_output(cmd, shell=True).decode('utf-8')
-    fname_list = fnames.split('\n')
-    i = 0
-    for name in fname_list:
-        if '/' not in name: del fname_list[i]
-        else: continue
-        i += 1
-    print(fname_list)
-    return fname_list
 
 def insert_flag(flag, fname, line_no = None):
     try:
@@ -28,11 +15,9 @@ def insert_flag(flag, fname, line_no = None):
         new_lines = []
 
         if line_no == None: 
-            with open(fname, 'ba+') as f:
-                f.write(flag)
+            with open(fname, 'ba+') as f: f.write(flag)
         else: 
-            with open(fname, 'br') as f:
-                lines = f.readlines()
+            with open(fname, 'br') as f: lines = f.readlines()
         
             lines[line_no - 1]  =+ flag
             new_lines = b''.join(lines)
@@ -52,10 +37,8 @@ def remove_flag(flag, old_file, new_file):
         with open(old_file,'rb') as f:lines = f.readlines()
 
         for line in lines:
-            if flag not in line: 
-                new_lines.append(line)
-            else: 
-                continue
+            if flag not in line: new_lines.append(line)
+            else: continue
         
         data = b''.join(new_lines)
 
@@ -64,30 +47,13 @@ def remove_flag(flag, old_file, new_file):
     except Exception as E: 
         print(E, flag)
 
-def find_files(base_path = '/', fname_flag = 'crazy_flag'):
-#def find_files(base_path = '/', fname_flag = False):
-    all_paths = []
-    valid_paths = []
-    for paths, dirs, files in walk(base_path):
-        all_paths.append(paths) 
-    #if fname_flag == False: 
-    #    return all_paths
-    else:
-        for fname in all_paths:
-            print(type(fname))
-            if (fname_flag in fname) or (fname_flag == fname):
-                valid_paths.append(fname)
-            else: 
-                continue
-        pprint(valid_paths)
-        return valid_paths
+def find_files(base_path = '/', flag = 'find_me'):
+    paths = []
+    for dirpath, dirs, files in walk(base_path):
+        for fname in files:
+            if flag not in fname: continue
+            paths.append('/'.join([dirpath, fname]))
+    return paths
 
 if __name__ == '__main__':
-    #flag = argv[1]
-    #old_file = '../tests/other_log.log'
-    #new_file = '../tests/new_other_log.log'
-    #remove_flag(flag, old_file, new_file)
-    
-    #base_path = argv[1]
-    #flag = argv[2]
-    find_files()
+    pprint(find_files())
--- file_manip/speed_test.sh
+++ file_manip/speed_test.sh
@@ -2,5 +2,6 @@ iters=$1
 outfile='./speed_results.txt'
 echo 'os.walk ' >> $outfile
 python -m timeit -n $iters 'from file_manip import find_files' 'find_files()' >> $outfile
-echo 'system find ' >> $outfile
-python -m timeit -n $iters 'from file_manip import _find_files' '_find_files()' >> $outfile
+#echo 'system find ' >> $outfile
+#python -m timeit -n $iters 'from file_manip import _find_files' '_find_files()' >> $outfile
+cat $outfile
